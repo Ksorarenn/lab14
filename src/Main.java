a52128a883caf36fafa813453bce690b06e1c7c1
Ksorarenn <Ksor@renn>
2025-04-02 10:32:32 +0300

    fixed

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Введите строку: ");
         java.util.Scanner in = new Scanner(System.in);
         String text = in.nextLine();
-        String regex = "\\b(1?\\d?\\d\\.|2[0-4]\\d\\.|25[0-5]\\.){3}1?\\d?\\d|2[0-4]\\d|25[0-5]\\b";
+        String regex = "\\b(1?\\d?\\d|2[0-4]\\d|25[0-5])(\\.(1?\\d?\\d|2[0-4]\\d|25[0-5])){3}\\b";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         System.out.println("Найденный ip-адрес: ");
